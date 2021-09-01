28b3e1403fc6ce857bc41f494cf4974f5c0d8a19
MladenLM <mladenlamesevic@gmail.com>
2021-09-01 13:56:32 +0200

    adding-room
    We added two rooms and functionality to move between them

---
+++ My project/Assets/Scripts/RoomNavigation.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomNavigation : MonoBehaviour
+{
+    public Room currentRoom;
+
+    private GameController controller;
+
+    private void Awake()
+    {
+        controller = GetComponent<GameController>();
+    }
+
+    public void UnpackExitsInRoom()
+    {
+        for (int i = 0; i < currentRoom.exits.Length; i++)
+        {
+            controller.interactionDescriptionsInRoom.Add(currentRoom.exits[i].exitDescription);
+        }
+    }
+}
